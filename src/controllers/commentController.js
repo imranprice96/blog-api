52b98a5abece5ea87aad720d520a51892f599414
pricei <pricei@Xubuntu.myguest.virtualbox.org>
2024-01-27 22:12:46 +1300

    finishing controls for all routes

--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -3,25 +3,42 @@ const Post = require("../models/post");
 
 // GET - fetch all comments for a post
 exports.comment_list = async (req, res, next) => {
-  const [post, comments] = await Promise.all([
-    Post.findById(req.params.id).exec(),
-    Comment.find({ post: req.params.id }).exec(),
-  ]);
-  if (genre === null) {
-    const err = new Error("Comments not found");
-    err.status = 404;
-    return next(err);
+  try {
+    const [post, comments] = await Promise.all([
+      Post.findById(req.params.postid).exec(),
+      Comment.find({ post_id: req.params.postid }).exec(),
+    ]);
+    res.json({ post: post, comments: comments });
+  } catch {
+    res.status(404).json({ error: "404 not found" });
+    return next();
   }
-
-  res.json({ post: post, comments: comments });
 };
 
 // POST - create new comment for a post
-exports.comment_create = (req, res, next) => {
-  res.send("not yet implemented");
+exports.comment_create = async (req, res, next) => {
+  try {
+    const comment = new Comment({
+      //validaiton
+      post_id: req.params.postid,
+      username: req.body.username,
+      text: req.body.text,
+    });
+    await comment.save();
+    res.json(comment);
+  } catch (error) {
+    res.send(error);
+    return next();
+  }
 };
 
 // DELETE - delete a comment
-exports.comment_delete = (req, res, next) => {
-  res.send("not yet implemented");
+exports.comment_delete = async (req, res, next) => {
+  try {
+    await Comment.findByIdAndDelete(req.params.commentid);
+    res.send({ success: true });
+  } catch (error) {
+    res.send(error);
+    return next();
+  }
 };
--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -7,9 +7,8 @@ exports.post_list = async (req, res, next) => {
   const allPosts = await Post.find().sort({ createdAt: 1 }).exec();
 
   if (allPosts === null) {
-    const err = new Error("Posts not found");
-    err.status = 404;
-    return next(err);
+    res.status(404).json({ error: "Posts not found" });
+    return next();
   }
 
   res.json({ posts: allPosts });
@@ -26,16 +25,37 @@ exports.post_create = async (req, res, next) => {
 };
 
 // GET - fetch single post
-exports.post_detail = (req, res, next) => {
-  res.send("not yet implemented");
+exports.post_detail = async (req, res, next) => {
+  const post = await Post.findById(req.params.postid).exec();
+  if (post === null) {
+    res.status(404).json({ error: "Post not found" });
+    return next();
+  }
+  res.json(post);
 };
 
 // PUT - update a post
-exports.post_update = (req, res, next) => {
-  res.send("not yet implemented");
+exports.post_update = async (req, res, next) => {
+  try {
+    //TODO validation - serverside or front end????
+    const filter = { _id: req.params.postid };
+    const update = { $set: req.body };
+    await Post.updateOne(filter, update);
+    res.send({ success: true });
+  } catch {
+    res.status(404).json({ error: "Post not found" });
+    return next();
+  }
 };
 
-// DELETE - delete a post
-exports.post_delete = (req, res, next) => {
-  res.send("not yet implemented");
+// DELETE - delete a post and its comments
+exports.post_delete = async (req, res, next) => {
+  try {
+    await Post.findByIdAndDelete(req.params.postid);
+    await Comment.deleteMany({ post_id: req.params.postid });
+    res.send({ success: true });
+  } catch (error) {
+    res.send(error);
+    next();
+  }
 };
