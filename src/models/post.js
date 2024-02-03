fc20f437907267c27097a28f8a3d7778a527497d
pricei <pricei@Xubuntu.myguest.virtualbox.org>
2024-02-03 18:33:45 +1300

    Updating post model

--- src/models/post.js
+++ src/models/post.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const PostSchema = new Schema(
   {
     title: { type: String, required: true, maxLength: 100 },
-    text: { type: String, required: true, maxLength: 1024 },
+    text: { type: String, required: true, maxLength: 5120 },
     published: { type: Boolean, default: false },
     // profile_image: blob
   },
